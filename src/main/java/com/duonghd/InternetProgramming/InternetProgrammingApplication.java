b883b8315bd686298bb54625e08b15fa2dbd47ba
= <=>
2022-11-08 13:24:09 +0700

    D cm

---
+++ src/main/java/com/duonghd/InternetProgramming/InternetProgrammingApplication.java
@@ -0,0 +1,13 @@
+package com.duonghd.InternetProgramming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternetProgrammingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternetProgrammingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/duonghd/InternetProgramming/InternetProgrammingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duonghd.InternetProgramming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternetProgrammingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
